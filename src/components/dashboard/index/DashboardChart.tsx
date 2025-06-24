9b80f30879c7a7f934050cbd440785f7f4734ba8
Lisandro Prada Toledo <lisandro.prada@gmail.com>
2025-06-24 18:22:26 -0300

    Cambio de color base FF3F25 a  29aae2

--- src/components/dashboard/index/DashboardChart.tsx
+++ src/components/dashboard/index/DashboardChart.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { Bar } from 'react-chartjs-2';
+'use client';
+import {Bar} from 'react-chartjs-2';
 import 'chart.js/auto';
 
 const BarChart = () => {
@@ -9,12 +9,12 @@ const BarChart = () => {
     datasets: [
       {
         label: 'Total View',
-        width: "14px",
-        backgroundColor: '#ff6725',
-        borderColor: '#ff6725',
+        width: '14px',
+        backgroundColor: '#29aae2',
+        borderColor: '#29aae2',
         borderWidth: 1,
-        hoverBackgroundColor: '#ff6725',
-        hoverBorderColor: '#ff6725',
+        hoverBackgroundColor: '#29aae2',
+        hoverBorderColor: '#29aae2',
         data: [20000, 12000, 6000, 16000, 10000, 5000, 11000],
       },
     ],
@@ -38,5 +38,3 @@ const BarChart = () => {
 };
 
 export default BarChart;
-
-
